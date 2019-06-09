f028fea25adc2088a4290db32a4ee804ef9641ae
Azat <altunbaew.a@yandex.ru>
2019-06-09 21:20:27 +0400

    создание UI

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace ClientWPF
 {
@@ -20,9 +8,38 @@ namespace ClientWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isConnected = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if(isConnected)
+            {
+                Disconnected();
+            }
+            else
+            {
+                Connected();
+            }
+        }
+        public void Connected()
+        {
+            tbUserName.IsEnabled = false;
+            if (!isConnected)
+            {
+                isConnected = true;
+            }
+        }
+        public void Disconnected()
+        {
+            tbUserName.IsEnabled = true;
+            if (isConnected)
+            {
+                isConnected = false;
+            }
         }
     }
 }
